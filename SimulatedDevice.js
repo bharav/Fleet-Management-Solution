65cf3f4fc0e7b6d7eccc39924038849af4510e01
bharav <vivek.bharatia@emc.com>
2016-03-23 16:36:27 +0530

    changes pushed

--- SimulatedDevice.js
+++ SimulatedDevice.js
@@ -46,51 +46,51 @@ let truck1SimulatedData = [
     
 let truck2SimulatedData = [
      {OutsideTemp: 23, ContainerTemp: -5, lat:12.999680000000001,lng:77.68169, tirepressure: [50, 50, 50, 50], fuel: 45.0, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -5, lat:12.998650000000001,lng:77.68235, tirepressure: [50, 50, 50, 50], fuel: 45.0, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -5, lat:12.99797,lng:77.68277, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -5, lat:12.996580000000002,lng:77.68360000000001, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:12.99577,lng:77.68415, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:12.994140000000002,lng:77.68535, tirepressure: [50, 50, 50, 50], fuel: 44.8, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:12.99327,lng:77.68597000000001, tirepressure: [50, 50, 50, 50], fuel: 44.8, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:12.992780000000002,lng:77.68638, tirepressure: [50, 50, 50, 50], fuel: 44.7, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:12.98999,lng:77.68849, tirepressure: [50, 50, 50, 50], fuel: 44.7, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -3, lat:12.989790000000001,lng:77.68862, tirepressure: [50, 50, 50, 50], fuel: 44.6, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -5, lat:12.998650000000001,lng:77.68235, tirepressure: [50, 50, 50, 50], fuel: 45.0, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -5, lat:12.99797,lng:77.68277, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -5, lat:12.996580000000002,lng:77.68360000000001, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:12.99577,lng:77.68415, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:12.994140000000002,lng:77.68535, tirepressure: [50, 50, 50, 50], fuel: 44.8, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:12.99327,lng:77.68597000000001, tirepressure: [50, 50, 50, 50], fuel: 44.8, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:12.992780000000002,lng:77.68638, tirepressure: [50, 50, 50, 50], fuel: 44.7, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:12.98999,lng:77.68849, tirepressure: [50, 50, 50, 50], fuel: 44.7, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -3, lat:12.989790000000001,lng:77.68862, tirepressure: [50, 50, 50, 50], fuel: 44.6, speed: 50},
 
-    {OutsideTemp: 23, ContainerTemp: -3, lat:12.98723,lng:77.6901, tirepressure: [50, 50, 50, 50], fuel: 44.6, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -3, lat:12.98502,lng:77.69145, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -3, lat:12.98291,lng:77.69276, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:12.982320000000001,lng:77.69314, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:12.981050000000002,lng:77.69395, tirepressure: [50, 50, 50, 50], fuel: 44.4, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:12.979170000000002,lng:77.69509000000001, tirepressure: [50, 50, 50, 50], fuel: 44.4, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:12.978280000000002,lng:77.69563000000001, tirepressure: [50, 50, 50, 50], fuel: 44.3, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: +0, lat:12.97644,lng:77.69669, tirepressure: [50, 50, 50, 50], fuel: 44.2, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: +1, lat:12.97483,lng:77.69761000000001, tirepressure: [50, 50, 50, 50], fuel: 44.1, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:12.972940000000001,lng:77.69874, tirepressure: [50, 50, 50, 50], fuel: 44.1, speed: 50} 
+    {OutsideTemp: 23, ContainerTemp: -3, lat:12.98723,lng:77.6901, tirepressure: [50, 50, 50, 50], fuel: 44.6, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -3, lat:12.98502,lng:77.69145, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -3, lat:12.98291,lng:77.69276, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:12.982320000000001,lng:77.69314, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:12.981050000000002,lng:77.69395, tirepressure: [50, 50, 50, 50], fuel: 44.4, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:12.979170000000002,lng:77.69509000000001, tirepressure: [50, 50, 50, 50], fuel: 44.4, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:12.978280000000002,lng:77.69563000000001, tirepressure: [50, 50, 50, 50], fuel: 44.3, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: +0, lat:12.97644,lng:77.69669, tirepressure: [50, 50, 50, 50], fuel: 44.2, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: +1, lat:12.97483,lng:77.69761000000001, tirepressure: [50, 50, 50, 50], fuel: 44.1, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:12.972940000000001,lng:77.69874, tirepressure: [50, 50, 50, 50], fuel: 44.1, speed: 50} 
     ];
     
     
 let truck3SimulatedData = [
     {OutsideTemp: 23, ContainerTemp: -5, lat:13.043040000000001,lng:77.61070000000001, tirepressure: [50, 50, 50, 50], fuel: 45.0, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -5, lat:13.04254,lng:77.61293, tirepressure: [50, 50, 50, 50], fuel: 45.0, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -5, lat:13.042380000000001,lng:77.61362000000001, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -5, lat:13.042240000000001,lng:77.61409, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:13.04194,lng:77.61459, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:13.041850000000002,lng:77.61476, tirepressure: [50, 50, 50, 50], fuel: 44.8, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:13.041760000000002,lng:77.61506, tirepressure: [50, 50, 50, 50], fuel: 44.8, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:13.04156,lng:77.61549000000001, tirepressure: [50, 50, 50, 50], fuel: 44.7, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -4, lat:13.04137,lng:77.61605, tirepressure: [50, 50, 50, 50], fuel: 44.7, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -3, lat:13.041150000000002,lng:77.61675000000001, tirepressure: [50, 50, 50, 50], fuel: 44.6, speed: 50},
-    
+    {OutsideTemp: 23, ContainerTemp: -5, lat:13.04254,lng:77.61293, tirepressure: [50, 50, 50, 50], fuel: 45.0, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -5, lat:13.042380000000001,lng:77.61362000000001, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -5, lat:13.042240000000001,lng:77.61409, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:13.04194,lng:77.61459, tirepressure: [50, 50, 50, 50], fuel: 44.9, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:13.041850000000002,lng:77.61476, tirepressure: [50, 50, 50, 50], fuel: 44.8, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:13.041760000000002,lng:77.61506, tirepressure: [50, 50, 50, 50], fuel: 44.8, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:13.04156,lng:77.61549000000001, tirepressure: [50, 50, 50, 50], fuel: 44.7, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -4, lat:13.04137,lng:77.61605, tirepressure: [50, 50, 50, 50], fuel: 44.7, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -3, lat:13.041150000000002,lng:77.61675000000001, tirepressure: [50, 50, 50, 50], fuel: 44.6, speed: 50},
+    
     {OutsideTemp: 23, ContainerTemp: -3, lat:13.04105,lng:77.61718, tirepressure: [50, 50, 50, 50], fuel: 44.6, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -3, lat:13.041060000000002,lng:77.61732, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -3, lat:13.04109,lng:77.61759, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:13.041110000000002,lng:77.61771, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:13.04113,lng:77.61795000000001, tirepressure: [50, 50, 50, 50], fuel: 44.4, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:13.04118,lng:77.61847, tirepressure: [50, 50, 50, 50], fuel: 44.4, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:13.04119,lng:77.61901, tirepressure: [50, 50, 50, 50], fuel: 44.3, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: +0, lat:13.041250000000002,lng:77.61981, tirepressure: [50, 50, 50, 50], fuel: 44.2, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: +1, lat:13.041100000000002,lng:77.61981, tirepressure: [50, 50, 50, 50], fuel: 44.1, speed: 50},
-    {OutsideTemp: 23, ContainerTemp: -1, lat:13.041200000000002,lng:77.62109000000001, tirepressure: [50, 50, 50, 50], fuel: 44.1, speed: 50} 
+    {OutsideTemp: 23, ContainerTemp: -3, lat:13.041060000000002,lng:77.61732, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -3, lat:13.04109,lng:77.61759, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:13.041110000000002,lng:77.61771, tirepressure: [50, 50, 50, 50], fuel: 44.5, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:13.04113,lng:77.61795000000001, tirepressure: [50, 50, 50, 50], fuel: 44.4, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:13.04118,lng:77.61847, tirepressure: [50, 50, 50, 50], fuel: 44.4, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:13.04119,lng:77.61901, tirepressure: [50, 50, 50, 50], fuel: 44.3, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: +0, lat:13.041250000000002,lng:77.61981, tirepressure: [50, 50, 50, 50], fuel: 44.2, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: +1, lat:13.041100000000002,lng:77.61981, tirepressure: [50, 50, 50, 50], fuel: 44.1, speed: 50},
+    {OutsideTemp: 23, ContainerTemp: -1, lat:13.041200000000002,lng:77.62109000000001, tirepressure: [50, 50, 50, 50], fuel: 44.1, speed: 50} 
     ];
 
 client.open(function(err) {
@@ -107,7 +107,7 @@ client.open(function(err) {
         let truckDataIndex = 0;
         setInterval(function() {
             var dataJson = {};
-            Object.assign(dataJson, {deviceId: 'MyDeviceId', TruckDetail: 'KA03AA1111', ContainerId:"Cont1"}, truck1SimulatedData[truckDataIndex]);
+            Object.assign(dataJson, {deviceId: 'MyDeviceId', TruckDetail: 'KA03AA1111', ContainerId:"Cont1", drivername:"Mark Zukenbeirg", start: "Silk Board", end:'Hebbal',pic:'http://api.randomuser.me/portraits/thumb/men/14.jpg'}, truck1SimulatedData[truckDataIndex]);
             var data = JSON.stringify(dataJson);
             var message = new Message(data);
             message.properties.add('messageType', 'interactive');
@@ -121,7 +121,7 @@ client.open(function(err) {
         let truckDataIndex = 0;
         setInterval(function() {
             var dataJson = {};
-            Object.assign(dataJson, {deviceId: 'MyDeviceId', TruckDetail: 'KA03AA2222', ContainerId:"Cont1"}, truck2SimulatedData[truckDataIndex]);
+            Object.assign(dataJson, {deviceId: 'MyDeviceId', TruckDetail: 'KA03AA2222', ContainerId:"Cont1", drivername:"Marko Freytag", start: "Electronic City", end:'Airport',pic:'http://api.randomuser.me/portraits/thumb/men/38.jpg'}, truck2SimulatedData[truckDataIndex]);
             var data = JSON.stringify(dataJson);
             var message = new Message(data);
             message.properties.add('messageType', 'interactive');
@@ -135,7 +135,7 @@ client.open(function(err) {
         let truckDataIndex = 0;
         setInterval(function() {
             var dataJson = {};
-            Object.assign(dataJson, {deviceId: 'MyDeviceId', TruckDetail: 'KA03AA3333', ContainerId:"Cont1"}, truck3SimulatedData[truckDataIndex]);
+            Object.assign(dataJson, {deviceId: 'MyDeviceId', TruckDetail: 'KA03AA3333', ContainerId:"Cont1", drivername:"Dieter Gruenewald", start: "Hosur", end:'Airport',pic:'http://api.randomuser.me/portraits/thumb/men/39.jpg'}, truck3SimulatedData[truckDataIndex]);
             var data = JSON.stringify(dataJson);
             var message = new Message(data);
             message.properties.add('messageType', 'interactive');
--- models/truck.js
+++ models/truck.js
@@ -11,9 +11,11 @@ var truckSchema = new mongoose.Schema({
     deviceId: { type: String },
     ContainerId: { type: String },
     tirepressure: { type: Object },
-    createddate: { type: Date, required: true, default: Date.now }
-
-
+    createddate: { type: Date, required: true, default: Date.now },
+    drivername: { type: String },
+    start: { type: String },
+    end: {type: String},
+    pic: {type:String}
 });
 
 mongoose.model('truck', truckSchema);
